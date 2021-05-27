f7679a770a7f8d283d9856ea3bd1ea3d2d2acb1f
Lahiru Sandaruwan <lahirus250@gmail.com>
2021-05-27 14:24:38 +0530

    update pivot function

--- 1-sorting-and-searching-algorithms/quick-sort-2/pivot.js
+++ 1-sorting-and-searching-algorithms/quick-sort-2/pivot.js
@@ -6,7 +6,7 @@ function pivot(arr, start = 0, end = arr.length - 1) {
     // swap values
     const swap = (list, i, j) => [list[i], list[j]] = [list[j], list[i]];
 
-    for (let i = start + 1; i < arr.length; i++) {
+    for (let i = start + 1; i <= end; i++) {
         if (pivot > arr[i]) {
             swapIndex++;
             // swap current index value with swap index value
